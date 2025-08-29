244736020235e3a9b300bc013582542d799be41a
coutinho98 <mateuscouto98@outlook.com>
2025-08-29 04:07:37 -0300

    fix cors 2.0

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: 'https://moviefront-edvy.onrender.com',
-    credentials: true,
-  });
+app.enableCors({
+  origin: 'https://moviefront-edvy.onrender.com', 
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+});
   
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
